649e7139d40249eeef3bf02eb4d0074701ce225e
Delikarl Jean-Baptiste <djj5@students.uwf.edu>
2016-11-30 12:15:23 -0600

    First commit for the project

--- ParkingLot.c
+++ ParkingLot.c
@@ -3,58 +3,119 @@
 #include <string.h>
 #include <strings.h>
 
+#define bool int
+#define true 1
+#define false 0
+
 
 int main(int argc, char** argv) {
 
-   printf("what is your status?\n");
-   printf("1. Student \n2. Faculty or Staff \n3. Parking Police \n4. Management \n5. Exit \n");
-   //printf("-->");
-   int input;
-   scanf("%d", &input);
-   if(input<1 || input>5){
-      printf("Wrong status option (number) input, try again\n");
-      scanf("%d", &input);
-   }
+   bool exit1 = true;
    
-   switch(input){
-      case 1: printf("Student>\n");
-              printf("What would you like to do?\n");
-              printf("1. Get a permit?\n");
-              printf("2. Register a vehicle (up to 3)?\n");
-              printf("3. View citation(s)?\n");
-              printf("4. Go back?\n");
-              int input1;
-              scanf("%d", &input1);
-              break;
-      case 2: printf("Faculty/Staff>\n");
-              printf("What would you like to do?\n");
-              printf("1. Get a permit?\n");
-              printf("2. Register a vehicle (up to 3)?\n");
-              printf("3. View citation(s)?\n");
-              printf("4. Go back?\n");
-              int input2;
-              scanf("%d", &input2);
-              break;
-      case 3: printf("Parking police>\n");
-              printf("What would you like to do?\n");
-              printf("1. Search registry?\n");
-              printf("2. Issue a citation?\n");
-              printf("3. Go back?\n");
-              int input3;
-              scanf("%d", &input3);
-              break;
-      case 4: printf("Management>");
-              printf("What would you like to do?\n");
-              printf("1. View lot status?\n");
-              printf("2. Update lot status?\n");
-              printf("3. View/update citation?\n"); //if citation is found delete or update citation
-              printf("4. Go back?\n");
-              int input4;
-              scanf("%d", &input4);
-              break;
-      case 5: exit(0);
-      default: printf("Invalid input\n");
-               break;
+   while(exit1){
+      bool exit2 = true;
+      while(exit2){
+         printf("\nwhat is your status?\n");
+         printf("1. Student \n2. Faculty or Staff \n3. Parking Police \n4. Management \n5. Exit \n\n");
+         //printf("-->");
+         int input;
+         scanf("%d", &input);
+         if(input<1 || input>5){
+            printf("Wrong status option (number) input, try again\n");
+            scanf("%d", &input);
+         }
+         
+         switch(input){
+            case 1: printf("\n\nStudent>\n");
+                    printf("What would you like to do?\n");
+                    printf("1. Get a permit?\n");
+                    printf("2. Register a vehicle (up to 3)?\n");
+                    printf("3. View citation(s)?\n");
+                    printf("4. Go back?\n");
+                    int input1;
+                    scanf("%d", &input1);
+                    if(input1 == 4){
+                        exit2 = false;
+                    }
+                    if(input1 == 3){
+                    
+                    }
+                    if(input1 == 2){
+                    
+                    }
+                    if(input1 == 1){
+                    
+                    }
+                    break;
+            case 2: printf("\n\nFaculty/Staff>\n");
+                    printf("What would you like to do?\n");
+                    printf("1. Get a permit?\n");
+                    printf("2. Register a vehicle (up to 3)?\n");
+                    printf("3. View citation(s)?\n");
+                    printf("4. Go back?\n");
+                    int input2;
+                    scanf("%d", &input2);
+                    if(input2 == 4){
+                        exit2 = false;
+                    }
+                    if(input2 == 3){
+                    
+                    }
+                    if(input2 == 2){
+                    
+                    }
+                    if(input2 == 1){
+                    
+                    }
+                    break;
+            case 3: printf("\n\nParking police>\n");
+                    printf("What would you like to do?\n");
+                    printf("1. Search registry?\n");
+                    printf("2. Issue a citation?\n");
+                    printf("3. Go back?\n");
+                    int input3;
+                    scanf("%d", &input3);
+                    if(input3 == 3){
+                        exit2 = false;
+                    }
+                    if(input3 == 2){
+                    
+                    }
+                    if(input3 == 1){
+                    
+                    }
+                    break;
+            case 4: printf("\n\nManagement>\n");
+                    printf("What would you like to do?\n");
+                    printf("1. View lot status?\n");
+                    printf("2. Update lot status?\n");
+                    printf("3. Search registry?\n");
+                    printf("4. View/update citation?\n"); //if citation is found delete or update citation
+                    printf("5. Go back?\n");
+                    int input4;
+                    scanf("%d", &input4);
+                    if(input4 == 5){
+                        exit2 = false;
+                    }
+                    if(input4 == 4){
+                    
+                    }
+                    if(input4 == 3){
+                    
+                    }
+                    if(input4 == 2){
+                    
+                    }
+                    if(input4 == 1){
+                    
+                    }
+                    break;
+            case 5: exit(0);
+                    //exit1 = false;
+            default: printf("Invalid input\n");
+                     break;
+         }
+      }
    }
 
 
--- main.c
+++ main.c
@@ -3,18 +3,20 @@
 //  se2
 //
 //  Created by David Giampa on 2016-11-20.
-//  Copyright Â© 2016 David Giampa. All rights reserved.
+//  Copyright © 2016 David Giampa. All rights reserved.
 //
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+char input[1000];
 typedef struct vechile
 {
     char color[256];
     int year[256];
     char type_of_car[256];
-    int permit_ID[256];
+    int permit_ID[256];//switch to student id (to be same as permit id)
 }vechile;
 
 typedef struct student
@@ -26,7 +28,14 @@ typedef struct student
     vechile vechelis[3];
 }student;
 
-student*create_student( )
+student* create_student( )
+{
+    struct student*next;
+    struct student*back;
+    vechile vechelis[3];
+}student;
+/////////////////////////////////////////////////////dynamically allocates memory for new student/////////////////////////////////////////////
+student*create_student( int id)
 {
     student*create=malloc(sizeof(student));
   
@@ -37,18 +46,92 @@ student*create_student( )
     memset(create->vechelis->year, 0, 256);
     create->citation_balance=0.0;
     create->next=NULL;
-    create->ID=0;
+	create->ID=0;
     return create;
 }
 
-int main(int argc, const char * argv[]) {
+///                     ///////////////////////////////////   adds a new student to a double linked list/////////////////////////
+void add_student(student *old,int num)
+{
+    student *new=create_student(num);
+    old->next=new;
+    new->back=old;
+}
+///                     //////////////////////////////////  displays the double linked list ///////////////////////////////////////
+void display_students_id(student*info)
+{
+    while(info->back!=NULL)
+    {
+        printf("%d\n",info->ID);
+        info=info->back;
+    }
+      printf("%d\n",info->ID);
+    
+    while(info->next!=NULL)
+    {
+        printf("%d\n",info->ID);
+        info=info->next;
+    }
+    printf("%d\n",info->ID);
+}
+///                ///////////////  //////////////////////////////////  //////////////////////////////////  /////////
+///                 ////////////////  ////////////////   remove a student from the linked list//////////////  /////////
+///               ////////////////  //////////////////////////////////  //////////////////////////////////  /////////
 
-    student *p1=create_student();
-    p1->ID=234;
-    printf("%d",p1->ID);
-    p1->vechelis->color[0]='b';
-    printf("%c\n",p1->vechelis->color[0]);
-    free(p1);
+void delete_middle(student*remove)
+{
+    /*
+    printf("current %d\n",remove->ID);
+     printf("next %d\n",remove->next->ID);
+    printf("back %d \n",remove->back->ID);
+     */
+   
+    student*front_ptr=remove->next;
+    student*back_ptr=remove->back;
+    
+    front_ptr->back=remove->back;
+    back_ptr->next=front_ptr;
+    
+    remove->next=NULL;
+    remove->back=NULL;
+    free(remove);
+}
+student* find_student(student*remove, int target_num)
+{
+    while (remove!=NULL) {
+        
+        if(remove->ID==target_num)
+        {
+            delete_middle(remove);
+            return remove;
+            break;
+        }
+        remove=remove->back;
+    }
+    return NULL;
+}
+void remove_student(student*remove,int kill)
+{
+    remove=find_student(remove, kill);
+}
+
+int main(int argc, const char * argv[])
+{
+    student* info;
+    info=create_student(10);
+   
+    add_student(info,6);
+    info=info->next;
+    
+    add_student(info,7);
+    info=info->next;
+    
+    add_student(info,11);
+    info=info->next;
+
+    remove_student(info,6);
+     remove_student(info,7);
+   display_students_id(info);
     
     return 0;
 }
